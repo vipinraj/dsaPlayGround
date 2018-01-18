ae16cf2a8d88110584b8a4ad24810c4af3a7b690
vipin <vipin.mscis3@iiitmk.ac.in>
2018-01-18 14:37:14 +0000

    searching

--- src/epi/searching/KthLargestElement.java
+++ src/epi/searching/KthLargestElement.java
@@ -14,7 +14,7 @@ public class KthLargestElement {
   public static void main(String[] args) { 
     List<Integer> arr = new ArrayList<>(Arrays.asList(3,2,4,18,1,9,6,10));
     
-    System.out.println(kthLargest(arr, 3));
+    System.out.println(kthLargest(arr, 2));
   }
   
   public static Integer kthLargest(List<Integer> a, int k) {
@@ -29,13 +29,12 @@ public class KthLargestElement {
         System.out.println("M: " + m);
         System.out.println("[" + l + ", " + r + "]");
         pivotIdx = partitionByM(a, l, r, m);
-        if (r - pivotIdx == k - 1) {
+        if (pivotIdx == k - 1) {
             return a.get(pivotIdx);
-        } else if (r - pivotIdx < k - 1) {
+        } else if (pivotIdx < k - 1) {
             l =  pivotIdx + 1;      
         } else {
             r = pivotIdx - 1;
-            k = k - (r - pivotIdx + 1);
         }
     }
     return null;
@@ -49,7 +48,7 @@ public class KthLargestElement {
       Collections.swap(a, m, r);
       System.out.println(a);
       while (i < r) {
-          if (a.get(i) < mElement) {
+          if (a.get(i) > mElement) {
               Collections.swap(a, pos, i);
               pos++;
           }
