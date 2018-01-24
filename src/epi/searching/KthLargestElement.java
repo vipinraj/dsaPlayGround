be325482f6a00240325bc997cac5f80e17b2b838
vipin <vipin.mscis3@iiitmk.ac.in>
2018-01-24 15:33:45 +0000

    searching

--- src/epi/searching/KthLargestElement.java
+++ src/epi/searching/KthLargestElement.java
@@ -21,6 +21,19 @@ public class KthLargestElement {
     System.out.println(getMedian(arr));
     arr = new ArrayList<>(Arrays.asList(1,2,3));
     System.out.println(getMedian(arr));
+    
+    List<Element> elemArr = new ArrayList<>();
+    elemArr.add(new Element(2, 'a'));
+    elemArr.add(new Element(3, 'a'));
+    elemArr.add(new Element(2, 'b'));
+    elemArr.add(new Element(0, 'a'));
+    elemArr.add(new Element(8, 'a'));
+    elemArr.add(new Element(4, 'a'));
+    elemArr.add(new Element(7, 'a'));
+    elemArr.add(new Element(7, 'b'));
+    elemArr.add(new Element(7, 'c'));
+    elemArr.add(new Element(1, 'a'));
+    System.out.println(ktheLargestWithDuplicates(elemArr, 2));
   }
   
   public static Integer kthLargest(List<Integer> a, int k) {
@@ -92,5 +105,76 @@ public class KthLargestElement {
       }
       return 0;
   }
- 
+  
+  // stable sorting not implemented
+  public static Element ktheLargestWithDuplicates(List<Element> a, int k){
+      
+      int left = 0;
+      int right = a.size() - 1;
+      
+      while (left <= right) {
+        Random r = new Random();
+        int pivotIdx = r.nextInt(right - left + 1) + left;
+        int pivotIdxupdated = partitionByPivot(a, left, right, pivotIdx);
+        if (pivotIdxupdated == k - 1) {
+            return a.get(pivotIdxupdated);
+        } else if (pivotIdxupdated > k - 1) {
+            right = pivotIdxupdated - 1;
+        } else {
+            left = pivotIdxupdated + 1;
+        }
+      }
+      
+      return null;
+  }
+  
+  public static int partitionByPivot(List<Element> a, int left, int right, int pivotIdx) {
+      Integer swappedIdx = null;
+      if (a.get(pivotIdx).compareTo(a.get(right)) != 0) {
+        Collections.swap(a, pivotIdx, right);
+        swappedIdx = pivotIdx;
+      }
+      int pos = left;
+      Element pivotElem = a.get(right);
+      for (int i = left; i < right; i++) {
+          int compareResult = a.get(i).compareTo(pivotElem);
+          
+          if (i > pivotIdx) {
+              
+              if (a.get(i).compareTo(a.get(swappedIdx)) == 0) {
+                  Collections.swap(a, swappedIdx, i);
+                  swappedIdx = i;
+              }
+             
+              if (compareResult == 0) {
+                  Collections.swap(a, i, right);
+              }   
+          }
+          if (compareResult >= 0)  {
+              Collections.swap(a, pos++, i);
+              if (i == pivotIdx) {
+                  swappedIdx = pos - 1;
+              }
+          }
+      }
+      Collections.swap(a, pos, right);
+
+      return pos;
+  }
 }
+  class Element implements Comparable<Element> {
+      int key;
+      char val;
+      
+      Element(int key, char val) {
+          this.key = key;
+          this.val = val;
+      }
+      
+      public int compareTo(Element o){
+        return Integer.compare(this.key, o.key);
+      }
+      public String toString() {
+        return "Key: " + key + ", Val: " + val;
+      }
+  }
