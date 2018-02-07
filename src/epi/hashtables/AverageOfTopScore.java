d95a406889810ed02b4ac2b7de9c74703aeafedb
vipin <vipin.mscis3@iiitmk.ac.in>
2018-02-07 17:55:08 +0000

    hashtables

---
+++ src/epi/hashtables/AverageOfTopScore.java
@@ -0,0 +1,38 @@
+package epi.hashtables;
+import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AverageOfTopScore {
+    public static void main(String args[]) {
+        List<Object> nameScoreData = new ArrayList<>();
+        nameScoreData.add("adnan");
+        nameScoreData.add(100);
+        nameScoreData.add("amit");
+        nameScoreData.add(99);
+        nameScoreData.add("adnan");
+        nameScoreData.add(98);
+        nameScoreData.add("thl");
+        nameScoreData.add(90);
+        nameScoreData.add("adnan");
+        nameScoreData.add(10);
+        nameScoreData.add("amit");
+        nameScoreData.add(100);
+        nameScoreData.add("thl");
+        nameScoreData.add(99);
+        nameScoreData.add("thl");
+        nameScoreData.add(95);
+        nameScoreData.add("dd");
+        nameScoreData.add(100);
+        nameScoreData.add("dd");
+        nameScoreData.add(100);
+        nameScoreData.add("adnan");
+        nameScoreData.add(99);
+        String result = findStudentWithHighestBestOfThreeScores(nameScoreData.iterator());
+    }
+    
+    public static String findStudentWithHighestBestOfThreeScores(Iterator<Object> input) {
+        Prioo        
+        return null;
+    }
+}
