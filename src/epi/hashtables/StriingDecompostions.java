e2f9b0f8e92ab03d3a8bcacb72f1a5de4c4ebba9
Vipin <vipin.mscis3@iiitmk.ac.in>
2018-03-28 23:54:21 +0530

    hashtables

--- src/epi/hashtables/StriingDecompostions.java
+++ src/epi/hashtables/StriingDecompostions.java
@@ -5,53 +5,62 @@ import java.util.Set;
 import java.util.HashSet;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class StriingDecompostions {
   
   
   public static void main(String[] args) { 
-    String s = "barfoothefoobarman";
-    List<String> A = Arrays.asList("foo", "bar");
+    String s = "barfoothefoobarmanbarbarfoo";
+    List<String> A = Arrays.asList("foo","bar","bar");
+    System.out.println(getAllIndexes(s, A));
   }
   
   public static List<Integer> getAllIndexes(String s, List<String> words) {
-    int wordLen = words.get(0).length;
-    Map<String, List<Integer>> wordToIdx = new HashMap<>();
+    List<Integer> result = new ArrayList<Integer>();
+    Map<String, Integer> wordFrequency = new HashMap<String, Integer>();
     
-    for (int i = 0; i <= s.length - wordLen; i++) {
-      
-      if (wordToIdx.containsKey(s.substring(i, i + wordLen + 1))) {
-        wordToIdx.put(s.substring(i, i + wordLen + 1), wordToIdx.get(s.substring(i, i + wordLen + 1)).add(i));
+    for (String w : words) {
+      if (wordFrequency.containsKey(w)) {
+        wordFrequency.put(w, wordFrequency.get(w) + 1);
       } else {
-        List<Integer> indexes = Arrays.asList(i);
-        wordToIdx.put(s.substring(i, i + wordLen + 1), indexes);
+        wordFrequency.put(w, 1);
       }
     }
     
-    Set<String> words = new HashSet<>();
+    int wordLength = words.get(0).length();
     
-    for (String str : words) {
-      words.add(str);
+    for (int i = 0; i <= s.length() - wordLength * words.size(); i++) {
+      if (matchAllWordsInDict(s, i, wordFrequency, words.size(), wordLength)) {
+        result.add(i);
+      }
     }
     
-    for (String str : words) {
-     Set<String> wordsCopy = new  HashSet<>(words);
-     List<Integer> indexes = wordToIdx.get(str);
-     
-     if (indexes != null) {
-       wordsCopy.remove(str);
-       
-       for (Integer i : indexes) {
-         if () {
-         
-         }
-       }
-     }
-     
-    }
-    
+    return result;
   }
   
+  public static boolean matchAllWordsInDict(String s, int start, Map<String, Integer> dict, int noOfWords, int wordLen) {
+    Map<String, Integer> currentDict = new HashMap<String, Integer>();
+    
+    for (int i = 0; i < noOfWords; i++) {
+      String subStr = s.substring(start + (i * wordLen), start + (i * wordLen) + wordLen);
+      Integer freq = dict.get(subStr);
+      
+      if (freq == null) {
+        return false;
+      }
+      
+      // insert frequency
+      if (currentDict.containsKey(subStr)) {
+        currentDict.put(subStr, currentDict.get(subStr) + 1);
+      } else {
+        currentDict.put(subStr, 1);
+      }
+      
+      if (currentDict.get(subStr) > freq) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
-abcde
-   3 4   
