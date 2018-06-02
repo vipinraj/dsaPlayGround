ed6c2a8bdc005501e446eefb81b282c3739df3b8
vipinraj <dvipin4u@gmail.com>
2018-06-02 13:03:22 +0530

    Arrays

---
+++ src/epi/arrays/NextPermutation.java
@@ -0,0 +1,77 @@
+package epi.arrays;
+
+public class NextPermutation {
+	public static void main(String args[]) {
+		int[] input = {6, 2, 1, 5, 4, 3, 0};
+
+		getNext(input);
+
+		for (int i = 0; i < input.length; i++) {
+			System.out.print(input[i] + ", ");
+		}
+
+		System.out.println();
+
+		int[] input2 = {6, 2, 3, 0, 1, 4, 5};
+
+		getNext(input2);
+
+		for (int i = 0; i < input2.length; i++) {
+			System.out.print(input2[i] + ", ");
+		}
+
+		System.out.println();
+
+		int[] input3 = {6, 2, 1};
+
+		int[] result = getNext(input3);
+
+		for (int i = 0; i < result.length; i++) {
+			System.out.print(result[i] + ", ");
+		}
+
+	}
+
+	public static int[] getNext(int[] permutation) {
+
+		// find largest increasing subsequence starting from right most element.
+		int size = permutation.length - 1;
+
+		int i;
+		for ( i = size - 1; i >= 0; i--) {
+			if (permutation[i] < permutation[i + 1]) {
+				break;
+			}
+		}
+
+		// this is the maximum permutation
+		if (i == -1) {
+			return new int[]{};
+		}
+
+		// find the smallest element in increasing sequence which is larger than permutation[i];
+		// then swap them;
+		for (int j = size; j > i; j--) {
+			if (permutation[j] > permutation[i]) {
+				// swap them
+				swap(permutation, i, j);
+				break;
+			}
+		}
+
+		// reverse the sub array [i+1, size]
+		int k = size;
+		for (int j = i + 1; j < i + 1 + (size + 1 - (i + 1))/2; j++ ) {
+			swap(permutation, j, k--);
+		}
+
+		return permutation;
+
+	}
+
+	public static void swap(int arr[], int i, int j) {
+		int t = arr[i];
+		arr[i] = arr[j];
+		arr[j] = t;
+	}
+}
