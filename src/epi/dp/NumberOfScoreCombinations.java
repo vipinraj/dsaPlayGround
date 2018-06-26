98134fe38b923379153da11d13b1e84113831e5a
vipinraj <dvipin4u@gmail.com>
2018-06-27 00:20:04 +0530

    dp

--- src/epi/dp/NumberOfScoreCombinations.java
+++ src/epi/dp/NumberOfScoreCombinations.java
@@ -9,8 +9,10 @@ public class NumberOfScoreCombinations {
 		int finalScore = 12;
 		Integer[][] dp = new Integer[finalScore + 1][scores.length];
 		System.out.println(countCombinations(scores, finalScore, 0, dp));
+		System.out.println(countCombinationsDp1(scores, finalScore));
 	}
 	
+	// recursive
 	public static int countCombinations(int[] scores, int targetScore, int i, Integer[][] dp) {
 		
 		if (targetScore == 0) {
@@ -32,5 +34,42 @@ public class NumberOfScoreCombinations {
 		}
 		
 		return dp[targetScore][i];
+	}
+
+	// dp - n^3 - inefficient
+	public static int countCombinationsDp1(int[] scores, int targetScore) {
+		int dp[][] = new int[scores.length][targetScore + 1];
+
+
+		for (int i = 0; i <= targetScore; i++) {
+			for (int j = 0; j < scores.length; j++) {
+				int combinations = 0;
+				for (int k = 0; i - k * scores[j] >= 0 && j != 0; k++ ) {
+					combinations += dp[j - 1][i - k * scores[j]];
+				}
+
+				if (j == 0) {
+					if (i >= scores[j] && i % scores[j] == 0) {
+						combinations = 1;
+					} else if (i == 0) {
+						combinations = 1;
+					}
+				}
+
+				dp[j][i] = combinations;
+			}
+		}
+		print(dp);
+		return dp[scores.length - 1][targetScore];
+	}
+
+	public static void print(int[][] arr) {
+		System.out.println();
+		for (int i = 0; i < arr.length; i++ ) {
+			for (int j = 0; j < arr[0].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
 }
