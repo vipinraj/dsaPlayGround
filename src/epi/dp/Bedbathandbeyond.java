3cc31bcaf9735a52327ed49a33818572b72787bd
vipin <vipin.mscis3@iiitmk.ac.in>
2018-07-19 17:42:05 +0000

    dp

--- src/epi/dp/Bedbathandbeyond.java
+++ src/epi/dp/Bedbathandbeyond.java
@@ -1,4 +1,37 @@
 package epi.dp;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Arrays;
 
 public class Bedbathandbeyond {
+    public static void main(String[] args) {
+        Set<String> dictionary = new HashSet<>(Arrays.asList(
+            "i", "like", "sam", "sung", "samsung", "mobile", "ice", 
+            "cream", "icecream", "man", "go", "mango"));
+        String input = "samsungandmangok";
+        System.out.println(checkIfDecomposable(dictionary, input, 0));
+    }
+    
+    
+    public static boolean checkIfDecomposable(Set<String> dictionary, String input, int indx) {
+        if (indx == input.length()) {
+            return true;
+        }
+        
+        int tmIndx = indx;
+        boolean found = false;
+        while (tmIndx < input.length()) {
+            tmIndx++;
+            if (dictionary.contains(input.substring(indx, tmIndx))) {
+                found = true;
+                break;
+            }
+        }
+        
+        if (found) {
+            return checkIfDecomposable(dictionary, input, tmIndx);
+        } else {
+            return false;
+        }
+    }
 }
