6a7bee62a8e01840c69629911d248589e556cf88
vipin <vipin.mscis3@iiitmk.ac.in>
2018-07-24 18:34:13 +0000

    dp

--- src/epi/dp/Bedbathandbeyond.java
+++ src/epi/dp/Bedbathandbeyond.java
@@ -4,16 +4,22 @@ import java.util.HashSet;
 import java.util.Arrays;
 
 public class Bedbathandbeyond {
+    public static int counter = 0;
     public static void main(String[] args) {
         Set<String> dictionary = new HashSet<>(Arrays.asList(
             "i", "like", "sam", "sung", "samsung", "mobile", "ice", 
             "cream", "icecream", "man", "go", "mango"));
+        Set<String> dictionary2 = new HashSet<>(Arrays.asList(
+            "bed", "bath", "and", "be", "beyond", "on"));
         String input = "samsungandmangok";
-        System.out.println(checkIfDecomposable(dictionary, input, 0));
+        String input2 = "bedbathandbeyond";
+        //System.out.println(checkIfDecomposable(dictionary, input, 0));
+        System.out.println(checkIfDecomposable(dictionary2, input2, 0));
     }
     
     
     public static boolean checkIfDecomposable(Set<String> dictionary, String input, int indx) {
+        System.out.println(++counter);
         if (indx == input.length()) {
             return true;
         }
@@ -23,15 +29,13 @@ public class Bedbathandbeyond {
         while (tmIndx < input.length()) {
             tmIndx++;
             if (dictionary.contains(input.substring(indx, tmIndx))) {
-                found = true;
-                break;
+                found = checkIfDecomposable(dictionary, input, tmIndx);;
+                if (found) {
+                    return true;
+                }
             }
         }
         
-        if (found) {
-            return checkIfDecomposable(dictionary, input, tmIndx);
-        } else {
-            return false;
-        }
+        return false;
     }
 }
