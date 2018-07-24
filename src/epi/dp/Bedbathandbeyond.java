d57f587b2021f5473ba1b2bec1a6a599844d28e4
vipinraj <dvipin4u@gmail.com>
2018-07-25 00:01:30 +0530

    dp

--- src/epi/dp/Bedbathandbeyond.java
+++ src/epi/dp/Bedbathandbeyond.java
@@ -2,14 +2,18 @@ package epi.dp;
 import java.util.Set;
 import java.util.HashSet;
 import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Bedbathandbeyond {
     public static void main(String[] args) {
         Set<String> dictionary = new HashSet<>(Arrays.asList(
             "i", "like", "sam", "sung", "samsung", "mobile", "ice", 
             "cream", "icecream", "man", "go", "mango"));
-        String input = "bedbathandbeyond";
+        String input = "ilikelikeimangoiii";
         System.out.println(checkIfDecomposable(dictionary, input, 0));
+        System.out.println(getDecomposition(dictionary, input));
     }
     
     
@@ -33,5 +37,48 @@ public class Bedbathandbeyond {
         } else {
             return false;
         }
+    }
+
+    public static List<String> getDecomposition(Set<String> dictionary, String input) {
+    	int [] lastLength = new int[input.length()];
+    	Arrays.fill(lastLength, -1);
+
+    	for (int i = 0; i < input.length(); i++) {
+    		if (dictionary.contains(input.substring(0, i + 1))) {
+    			lastLength[i] = i + 1;
+    		}
+
+    		if (lastLength[i] == -1) {
+
+    			for (int j = 0; j < i; j++) {
+    				if (lastLength[j] != -1 && dictionary.contains(input.substring(j + 1, i + 1))) {
+    					lastLength[i] = i - j;
+    					break;
+    				}
+    			}
+    		}
+    	}
+
+    	for (int i = 0; i < input.length(); i++) {
+    		System.out.print(lastLength[i] + " ");
+    	}
+
+    	List<String> output = new ArrayList<>();
+
+    	if (lastLength[input.length() - 1] != -1) {
+	    	
+
+    		int indx = input.length() - 1;
+
+	    	while (indx >= 0) {
+	    		
+    			output.add(input.substring(indx - lastLength[indx] + 1, indx + 1));
+    			indx = indx - lastLength[indx];
+    		
+	    	}
+	    	Collections.reverse(output);
+    	}
+
+    	return output;
     }
 }
