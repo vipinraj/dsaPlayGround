7bdbe8e9362f61fcef6db9e6667d6454ac06afd4
Vipin <dvipin4u@gmail.com>
2019-10-09 23:29:34 +0530

    Quick Sort
    Quick sort implementation. Works for duplicate keys also.

---
+++ src/coursera_algorithms_p1/week3/Quick.java
@@ -0,0 +1,74 @@
+package week3;
+
+import edu.princeton.cs.algs4.StdRandom;
+
+import java.util.Arrays;
+
+public class Quick {
+
+    private static void sort(Comparable[] a, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+
+        int i = partition(a, start, end);
+        System.out.println(i);
+        sort(a, start, i - 1);
+        sort(a, i + 1, end);
+    }
+
+    private static int partition(Comparable[] a, int start, int end) {
+        int lo = start + 1;
+        int hi = end;
+
+        while (lo <= hi) {
+            while (less(a[lo], a[start])) {
+                lo++;
+
+                if (lo > end) {
+                    break;
+                }
+            }
+
+            while (less(a[start], a[hi])) {
+                hi--;
+
+                if (hi < start + 1) {
+                    break;
+                }
+            }
+
+            if (lo >= hi) {
+                break;
+            }
+
+            exch(a, lo, hi);
+
+        }
+
+        exch(a, start, hi);
+        return hi;
+    }
+
+    private static void exch(Comparable[] a, int lo, int hi) {
+        Comparable l = a[lo];
+        a[lo] = a[hi];
+        a[hi] = l;
+    }
+
+    private static boolean less(Comparable a, Comparable b) {
+        // Note: used <= here
+        return a.compareTo(b) <= 0;
+    }
+
+    public static void sort(Comparable[] a) {
+        StdRandom.shuffle(a);
+        sort(a, 0, a.length - 1);
+    }
+    public static void main(String[] args) {
+        String[] items = "Z, Y, X, W, A, V, U, A, T, S, S, R, Q, P, O, N, M, L, K, J, I, H, G, F, E, D, C, B, A".split(", ");
+        Quick.sort(items);
+
+        System.out.println(Arrays.toString(items));
+    }
+}
